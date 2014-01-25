6347b116053d3bb896ee33d0d5582e5c749eb9a0
quicksilver71 <quicksilver71@live.it>
2014-01-25 14:16:59 +0100

    aggiunti controller

---
+++ ConfigWeightMeasure.h
@@ -0,0 +1,13 @@
+//
+//  ConfigWeightMeasure.h
+//  WeightMeasure
+//
+//  Created by MASSIMILIANO DI DOMENICO on 24/01/14.
+//  Copyright (c) 2014 MASSIMILIANO DI DOMENICO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigWeightMeasure : UIViewController
+
+@end
---
+++ ConfigWeightMeasure.m
@@ -0,0 +1,38 @@
+//
+//  ConfigWeightMeasure.m
+//  WeightMeasure
+//
+//  Created by MASSIMILIANO DI DOMENICO on 24/01/14.
+//  Copyright (c) 2014 MASSIMILIANO DI DOMENICO. All rights reserved.
+//
+
+#import "ConfigWeightMeasure.h"
+
+@interface ConfigWeightMeasure ()
+
+@end
+
+@implementation ConfigWeightMeasure
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
